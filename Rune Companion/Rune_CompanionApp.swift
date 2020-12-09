b97687a62a0dd745507ab86dce70c0cf4fb0c707
Elfuthark Apps <info@elfuthark.com>
2020-12-09 23:45:19 +0000

    Initial Commit

---
+++ Rune Companion/Rune_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rune_CompanionApp.swift
+//  Rune Companion
+//
+//  Created by Ryan Ball on 09/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Rune_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
